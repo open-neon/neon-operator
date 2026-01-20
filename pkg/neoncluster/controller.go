fe997342f9ea24a3d32e1154ee739225160c00c7
Yash Patel <yp969803@gmail.com>
2026-01-20 18:45:28 +0530

    feat: remove TODO comment from Reconcile function and add statefullset.go file with license

--- pkg/neoncluster/controller.go
+++ pkg/neoncluster/controller.go
@@ -52,11 +52,6 @@ func New(client client.Client, scheme *runtime.Scheme, logger *slog.Logger) (*Op
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the NeonCluster object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.22.4/pkg/reconcile
 func (r *Operator) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
