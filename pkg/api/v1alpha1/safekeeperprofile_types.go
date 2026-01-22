d08d3fe4bc478c6fca69515e1d9e61d31b9d34b0
Yash Patel <yp969803@gmail.com>
2026-01-22 11:10:44 +0530

    feat: add SafeKeeperProfile and StorageBrokerProfile APIs with DeepCopy methods and spec/status definitions

---
+++ pkg/api/v1alpha1/safekeeperprofile_types.go
@@ -0,0 +1,131 @@
+/*
+Copyright 2026.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	appsv1 "k8s.io/api/apps/v1"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	SafeKeeperProfileKind = "SafeKeeperProfile"
+	SafeKeeperProfileKey  = "safekeeperprofile"
+	SafeKeeperProfileName = "safekeeperprofiles"
+)
+
+// SafeKeeperProfileSpec defines the desired state of SafeKeeperProfile.
+// +k8s:openapi-gen=true
+type SafeKeeperProfileSpec struct {
+	CommonFields `json:",inline"`
+
+	// +kubebuilder:default=3
+	// +kubebuilder:validation:Minimum=3
+	// +optional
+	MinReplicas *int64 `json:"minReplicas,omitempty"`
+
+	// +kubebuilder:default=3
+	// +kubebuilder:validation:Minimum=3
+	// +optional
+	MaxReplicas *int64 `json:"maxReplicas,omitempty"`
+
+	// storage defines the storage used by SafeKeeper.
+	// +optional
+	Storage *StorageSpec `json:"storage,omitempty"`
+
+	// volumes allows the configuration of additional volumes on the output
+	// StatefulSet definition. Volumes specified will be appended to other
+	// volumes that are generated as a result of StorageSpec objects.
+	// +optional
+	Volumes []v1.Volume `json:"volumes,omitempty"`
+
+	// volumeMounts allows the configuration of additional VolumeMounts.
+	//
+	// VolumeMounts will be appended to other VolumeMounts in the 'safekeeper'
+	// container, that are generated as a result of StorageSpec objects.
+	// +optional
+	VolumeMounts []v1.VolumeMount `json:"volumeMounts,omitempty"`
+
+	// persistentVolumeClaimRetentionPolicy defines the field controls if and how PVCs are deleted during the lifecycle of a StatefulSet.
+	// The default behavior is all PVCs are retained.
+	// This is an alpha field from kubernetes 1.23 until 1.26 and a beta field from 1.26.
+	// It requires enabling the StatefulSetAutoDeletePVC feature gate.
+	//
+	// +optional
+	PersistentVolumeClaimRetentionPolicy *appsv1.StatefulSetPersistentVolumeClaimRetentionPolicy `json:"persistentVolumeClaimRetentionPolicy,omitempty"`
+}
+
+// SafeKeeperProfileStatus defines the observed state of SafeKeeperProfile.
+type SafeKeeperProfileStatus struct {
+	// For Kubernetes API conventions, see:
+	// https://github.com/kubernetes/community/blob/master/contributors/devel/sig-architecture/api-conventions.md#typical-status-properties
+
+	// conditions represent the current state of the SafeKeeperProfile resource.
+	// Each condition has a unique type and reflects the status of a specific aspect of the resource.
+	//
+	// Standard condition types include:
+	// - "Available": the resource is fully functional
+	// - "Progressing": the resource is being created or updated
+	// - "Degraded": the resource failed to reach or maintain its desired state
+	//
+	// The status of each condition is one of True, False, or Unknown.
+	// +listType=map
+	// +listMapKey=type
+	// +optional
+	Conditions []metav1.Condition `json:"conditions,omitempty"`
+}
+
+// +genclient
+// +k8s:openapi-gen=true
+// +kubebuilder:object:root=true
+// +kubebuilder:resource:categories="neon-operator",shortName="skp"
+// +kubebuilder:printcolumn:name="Min Replicas",type="integer",JSONPath=".spec.minReplicas",description="The minimum number of SafeKeeper replicas"
+// +kubebuilder:printcolumn:name="Max Replicas",type="integer",JSONPath=".spec.maxReplicas",description="The maximum number of SafeKeeper replicas"
+// +kubebuilder:printcolumn:name="Available",type="string",JSONPath=".status.conditions[?(@.type == 'Available')].status"
+// +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
+// +kubebuilder:subresource:status
+
+// SafeKeeperProfile is the Schema for the safekeeperprofiles API
+type SafeKeeperProfile struct {
+	metav1.TypeMeta `json:",inline"`
+
+	// metadata is a standard object metadata
+	// +optional
+	metav1.ObjectMeta `json:"metadata,omitzero"`
+
+	// spec defines the desired state of SafeKeeperProfile
+	// +required
+	Spec SafeKeeperProfileSpec `json:"spec"`
+
+	// status defines the observed state of SafeKeeperProfile
+	// +optional
+	Status SafeKeeperProfileStatus `json:"status,omitzero"`
+}
+
+// +kubebuilder:object:root=true
+
+// SafeKeeperProfileList contains a list of SafeKeeperProfile
+// +k8s:openapi-gen=true
+type SafeKeeperProfileList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitzero"`
+	Items           []SafeKeeperProfile `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&SafeKeeperProfile{}, &SafeKeeperProfileList{})
+}
---
+++ pkg/api/v1alpha1/storagebrokerprofile_types.go
@@ -0,0 +1,105 @@
+/*
+Copyright 2026.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	StorageBrokerProfileKind = "StorageBrokerProfile"
+	StorageBrokerProfileKey  = "storagebrokerprofile"
+	StorageBrokerProfileName = "storagebrokerprofiles"
+)
+
+// StorageBrokerProfileSpec defines the desired state of StorageBrokerProfile.
+// It will be deployed as a Deployment.
+// +k8s:openapi-gen=true
+type StorageBrokerProfileSpec struct {
+	CommonFields `json:",inline"`
+
+	// +kubebuilder:default=1
+	// +kubebuilder:validation:Minimum=1
+	// +optional
+	MinReplicas *int64 `json:"minReplicas,omitempty"`
+
+	// +kubebuilder:default=1
+	// +kubebuilder:validation:Minimum=1
+	// +optional
+	MaxReplicas *int64 `json:"maxReplicas,omitempty"`
+}
+
+// StorageBrokerProfileStatus defines the observed state of StorageBrokerProfile.
+type StorageBrokerProfileStatus struct {
+	// For Kubernetes API conventions, see:
+	// https://github.com/kubernetes/community/blob/master/contributors/devel/sig-architecture/api-conventions.md#typical-status-properties
+
+	// conditions represent the current state of the StorageBrokerProfile resource.
+	// Each condition has a unique type and reflects the status of a specific aspect of the resource.
+	//
+	// Standard condition types include:
+	// - "Available": the resource is fully functional
+	// - "Progressing": the resource is being created or updated
+	// - "Degraded": the resource failed to reach or maintain its desired state
+	//
+	// The status of each condition is one of True, False, or Unknown.
+	// +listType=map
+	// +listMapKey=type
+	// +optional
+	Conditions []metav1.Condition `json:"conditions,omitempty"`
+}
+
+// +genclient
+// +k8s:openapi-gen=true
+// +kubebuilder:object:root=true
+// +kubebuilder:resource:categories="neon-operator",shortName="sbp"
+// +kubebuilder:printcolumn:name="Min Replicas",type="integer",JSONPath=".spec.minReplicas",description="The minimum number of StorageBroker replicas"
+// +kubebuilder:printcolumn:name="Max Replicas",type="integer",JSONPath=".spec.maxReplicas",description="The maximum number of StorageBroker replicas"
+// +kubebuilder:printcolumn:name="Available",type="string",JSONPath=".status.conditions[?(@.type == 'Available')].status"
+// +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
+// +kubebuilder:subresource:status
+
+// StorageBrokerProfile is the Schema for the storagebrokerprofiles API
+type StorageBrokerProfile struct {
+	metav1.TypeMeta `json:",inline"`
+
+	// metadata is a standard object metadata
+	// +optional
+	metav1.ObjectMeta `json:"metadata,omitzero"`
+
+	// spec defines the desired state of StorageBrokerProfile
+	// +required
+	Spec StorageBrokerProfileSpec `json:"spec"`
+
+	// status defines the observed state of StorageBrokerProfile
+	// +optional
+	Status StorageBrokerProfileStatus `json:"status,omitzero"`
+}
+
+// +kubebuilder:object:root=true
+
+// StorageBrokerProfileList contains a list of StorageBrokerProfile
+// +k8s:openapi-gen=true
+type StorageBrokerProfileList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitzero"`
+	Items           []StorageBrokerProfile `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&StorageBrokerProfile{}, &StorageBrokerProfileList{})
+}
